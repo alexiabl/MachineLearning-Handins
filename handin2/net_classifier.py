715eb6906a3f9bd52b5c6ae3f557533bdd454bc6
alexiabl <a1b9l9d6>
2018-10-29 10:52:09 +0100

    handin2

---
+++ handin2/net_classifier.py
@@ -0,0 +1,354 @@
+import numpy as np
+import math
+
+def one_in_k_encoding(vec, k):
+    """ One-in-k encoding of vector to k classes 
+    
+    Args:
+       vec: numpy array - data to encode
+       k: int - number of classes to encode to (0,...,k-1)
+    """
+    n = vec.shape[0]
+    enc = np.zeros((n, k))
+    enc[np.arange(n), vec] = 1
+    return enc
+
+def softmax(X):
+    """ 
+    You can take this from handin I
+    Compute the softmax of each row of an input matrix (2D numpy array). 
+    
+    the numpy functions amax, log, exp, sum may come in handy as well as the keepdims=True option and the axis option.
+    Remember to handle the numerical problems as discussed in the description.
+    You should compute lg softmax first and then exponentiate 
+    
+    More precisely this is what you must do.
+    
+    For each row x do:
+    compute max of x
+    compute the log of the denominator sum for softmax but subtracting out the max i.e (log sum exp x-max) + max
+    compute log of the softmax: x - logsum
+    exponentiate that
+    
+    You can do all of it without for loops using numpys vectorized operations.
+
+    Args:
+        X: numpy array shape (n, d) each row is a data point
+    Returns:
+        res: numpy array shape (n, d)  where each row is the softmax transformation of the corresponding row in X i.e res[i, :] = softmax(X[i, :])
+    """
+    res = np.zeros(X.shape)
+    ### YOUR CODE HERE no for loops please
+    num = np.exp(X)
+    res = num / np.sum(num, axis=1, keepdims=True)
+    return res
+
+def relu(x):
+    """ Compute the relu activation function on every element of the input
+    
+        Args:
+            x: np.array
+        Returns:
+            res: np.array same shape as x
+        Beware of np.max and look at np.maximum
+    """
+    ### YOUR CODE HERE
+    #print(x)
+    res = np.maximum(x,0)
+    ### END CODE
+    return res
+
+def relu_derivative(x):
+     return 1 * (x > 0)
+
+def make_dict(W1, b1, W2, b2):
+    """ Trivial helper function """
+    return {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+
+
+def get_init_params(input_dim, hidden_size, output_size):
+    """ Initializer function using he et al Delving Deep into Rectifiers: Surpassing Human-Level Performance on ImageNet Classification
+
+    Args:
+      input_dim: int
+      hidden_size: int
+      output_size: int
+    Returns:
+       dict of randomly initialized parameter matrices.
+    """
+    W1 = np.random.normal(0, np.sqrt(2./(input_dim+hidden_size)), size=(input_dim, hidden_size))
+    b1 = np.zeros((1, hidden_size))
+    W2 = np.random.normal(0, np.sqrt(4./(hidden_size+output_size)), size=(hidden_size, output_size))
+    b2 = np.zeros((1, output_size))
+    return {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+
+  
+class NetClassifier():
+    
+    def __init__(self):
+        """ Trivial Init """
+        self.params = None
+        self.hist = None
+
+    def predict(self, X, params=None):
+        """ Compute class prediction for all data points in class X
+        
+        Args:
+            X: np.array shape n, d
+            params: dict of params to use (if none use stored params)
+        Returns:
+            np.array shape n, 1
+        """
+        if params is None:
+            params = self.params
+        pred = None
+        ### YOUR CODE HERE
+        z1 = np.dot(X,params['W1'])+params['b1']
+        a1 = relu(z1)
+        z2 = np.dot(a1, params['W2']) + params['b2']
+        pred = softmax(z2)
+        pred = np.argmax(pred,axis=1)
+        ### END CODE
+        return pred
+     
+    def score(self, X, y, params=None):
+        """ Compute accuracy of model on data X with labels y
+        
+        Args:
+            X: np.array shape n, d
+            y: np.array shape n, 1
+            params: dict of params to use (if none use stored params)
+
+        Returns:
+            np.array shape n, 1
+        """
+        if params is None:
+            params = self.params
+        acc = None
+        ### YOUR CODE HERE
+        prob = self.predict(X)
+        acc = np.mean(prob == y)
+        ### END CODE
+        return acc
+    
+    @staticmethod
+    def cost_grad(X, y, params, reg=0.0):
+        """ Compute cost and gradient of neural net on data X with labels y using weight decay parameter c
+        You should implement a forward pass and store the intermediate results 
+        and the implement the backwards pass using the intermediate stored results
+        
+        Use the derivative for cost as a function for input to softmax as derived above
+        
+        Args:
+            X: np.array shape n, self.input_size
+            y: np.array shape n, 1
+            params: dict with keys (W1, W2, b1, b2)
+            reg: float - weight decay regularization weight
+            params: dict of params to use for the computation
+        
+        Returns 
+            cost: scalar - average cross entropy cost
+            dict with keys
+            d_w1: np.array shape w1.shape, entry d_w1[i, j] = \partial cost/ \partial w1[i, j]
+            d_w2: np.array shape w2.shape, entry d_w2[i, j] = \partial cost/ \partial w2[i, j]
+            d_b1: np.array shape b1.shape, entry d_b1[1, j] = \partial cost/ \partial b1[1, j]
+            d_b2: np.array shape b2.shape, entry d_b2[1, j] = \partial cost/ \partial b2[1, j]
+            
+        """
+            
+        W1 = params['W1']
+        b1 = params['b1']
+        W2 = params['W2']
+        b2 = params['b2']
+        labels = one_in_k_encoding(y, W2.shape[1]) # shape n x k
+        
+        ### YOUR CODE HERE - FORWARD PASS - compute regularized cost and store relevant values for backprop
+        z1 = X.dot(W1) + b1
+        a1 = relu(z1)
+        z2 = a1.dot(W2) + b2
+        yy = softmax(z2)
+        
+        log_probs = -np.log(yy[range(len(X)), y])
+        data_loss = np.sum(log_probs)
+        data_loss += reg/2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
+        cost = 1/len(X) * data_loss
+        ### END CODE
+        
+        ### YOUR CODE HERE - BACKWARDS PASS - compute derivatives of all (regularized) weights and bias, store them in d_w1, d_w2' d_w2, d_b1, d_b2        
+        delta2 = yy
+        delta2 -= labels
+        d_w2 = np.dot(a1.T,delta2)
+        print ("d_w2 =",d_w2)
+        d_b2 = np.sum(delta2, axis=0, keepdims=True)
+        print ("d_b2 =",d_b2)
+        
+        delta1 = np.dot(delta2,W2.T) * relu_derivative(z1)
+        d_w1 = np.dot(X.T, delta1)
+        print ("d_w1 =",d_w1)
+        d_b1 = np.sum(delta1, axis=0)
+        print ("d_b1 =",d_b1)
+        ### END CODE
+        
+
+        print(cost)
+        # the return signature
+        return cost, {'d_w1': d_w1, 'd_w2': d_w2, 'd_b1': d_b1, 'd_b2': d_b2}
+        
+    def fit(self, X_train, y_train, X_val, y_val, init_params, batch_size=32, lr=0.1, reg=1e-4, epochs=30):
+        """ Run Mini-Batch Gradient Descent on data X, Y to minimize the in sample error (1/n)Cross Entropy for Neural Net classification
+        Printing the performance every epoch is a good idea to see if the algorithm is working
+    
+        Args:
+           X_train: numpy array shape (n, d) - the training data each row is a data point
+           y_train: numpy array shape (n,) int - training target labels numbers in {0, 1,..., k-1}
+           X_val: numpy array shape (n, d) - the validation data each row is a data point
+           y_val: numpy array shape (n,) int - validation target labels numbers in {0, 1,..., k-1}
+           init_params: dict - has initial setting of parameters
+           lr: scalar - initial learning rate
+           batch_size: scalar - size of mini-batch
+           epochs: scalar - number of iterations through the data to use
+
+        Sets: 
+           params: dict with keys {W1, W2, b1, b2} parameters for neural net
+           history: dict:{keys: train_loss, train_acc, val_loss, val_acc} each an np.array of size epochs of the the given cost after every epoch
+        """
+        
+        W1 = init_params['W1']
+        b1 = init_params['b1']
+        W2 = init_params['W2']
+        b2 = init_params['b2']
+        train_loss = np.empty(epochs)
+        train_acc = np.empty(epochs)
+        val_loss = np.empty(epochs)
+        val_acc = np.empty(epochs)
+        ### YOUR CODE HERE
+        self.params = init_params
+        for i in range(epochs): 
+            #Shuffle Data
+            shuff_X_train = np.copy(X_train)
+            shuff_y_train = np.copy(y_train) 
+            shuff_X_val = np.copy(X_val)
+            shuff_y_val = np.copy(y_val) 
+            rand = np.random.get_state()
+            np.random.shuffle(shuff_X_train)
+            np.random.set_state(rand)
+            np.random.shuffle(shuff_y_train)
+            np.random.set_state(rand)
+            np.random.shuffle(shuff_X_val)
+            np.random.set_state(rand)
+            np.random.shuffle(shuff_y_val)
+            n = len(shuff_X_train)
+            b = math.ceil(n/batch_size)
+            count=0
+            for j in range(b):
+                final = min(count+batch_size,n)
+                batchX_train = shuff_X_train[count:final]
+                batchY_train = shuff_y_train[count:final]
+                batchX_val = shuff_X_val[count:final]
+                batchY_val = shuff_y_val[count:final]
+                
+                cost_t, updates = self.cost_grad(batchX_train,batchY_train,self.params)
+                cost_v, updates_val = self.cost_grad(batchX_val,batchY_val,self.params)
+
+                updates['d_w1'] += reg * W1
+                updates['d_w2'] += reg * W2
+                W1 += lr * updates['d_w1']
+                W2 += lr * updates['d_w2']
+                b1 += lr * updates['d_b1']
+                b2 += lr * updates['d_b2']
+                count += batch_size  
+                train_loss = np.append(train_loss,cost_t)
+                val_loss = np.append(val_loss,cost_v)
+                train_acc = np.append(train_acc,self.score(batchX_train,batchY_train))
+                val_acc = np.append(val_acc,self.score(batchX_val,batchY_val))
+                
+        ### END CODE
+        # hist dict should look like this with something different than none
+        self.history = {
+            'train_loss': train_loss,
+            'train_acc': train_acc,
+            'val_loss': val_loss,
+            'val_acc': val_acc, 
+        }
+        ## self.params should look like this with something better than none, i.e. the best parameters found.
+
+def test_all():
+    input_dim = 7
+    hidden_size = 1
+    output_size = 3
+    nc = NetClassifier()
+    params = get_init_params(input_dim, hidden_size, output_size)
+
+    nc = NetClassifier()
+    X = np.random.randn(7, input_dim)
+    y = np.array([0, 1, 2, 0, 1, 2, 0])
+    print("X = ",X)
+    print("Testing relu")
+    res = relu(X)
+    cost_grad(X,y)
+    print(res)
+
+def numerical_grad_check(f, x, key):
+    """ Numerical Gradient Checker """
+    eps = 1e-6
+    h = 1e-5
+    # d = x.shape[0]
+    cost, grad = f(x)
+    grad = grad[key]
+    it = np.nditer(x, flags=['multi_index'])
+    while not it.finished:    
+        dim = it.multi_index    
+        print(dim)
+        tmp = x[dim]
+        x[dim] = tmp + h
+        cplus, _ = f(x)
+        x[dim] = tmp - h 
+        cminus, _ = f(x)
+        x[dim] = tmp
+        num_grad = (cplus-cminus)/(2*h)
+        # print('cplus cminus', cplus, cminus, cplus-cminus)
+        # print('dim, grad, num_grad, grad-num_grad', dim, grad[dim], num_grad, grad[dim]-num_grad)
+        assert np.abs(num_grad - grad[dim]) < eps, 'numerical gradient error index {0}, numerical gradient {1}, computed gradient {2}'.format(dim, num_grad, grad[dim])
+        it.iternext()
+
+def test_grad():
+    stars = '*'*5
+    print(stars, 'Testing  Cost and Gradient Together')
+    input_dim = 7
+    hidden_size = 1
+    output_size = 3
+    nc = NetClassifier()
+    params = get_init_params(input_dim, hidden_size, output_size)
+
+    nc = NetClassifier()
+    X = np.random.randn(7, input_dim)
+    y = np.array([0, 1, 2, 0, 1, 2, 0])
+
+    f = lambda z: nc.cost_grad(X, y, params, reg=1.0)
+    print('\n', stars, 'Test Cost and Gradient of b2', stars)
+    numerical_grad_check(f, params['b2'], 'd_b2')
+    print(stars, 'Test Success', stars)
+    
+    print('\n', stars, 'Test Cost and Gradient of w2', stars)
+    numerical_grad_check(f, params['W2'], 'd_w2')
+    print('Test Success')
+    
+    print('\n', stars, 'Test Cost and Gradient of b1', stars)
+    numerical_grad_check(f, params['b1'], 'd_b1')
+    print('Test Success')
+    
+    print('\n', stars, 'Test Cost and Gradient of w1', stars)
+    numerical_grad_check(f, params['W1'], 'd_w1')
+    print('Test Success')
+
+if __name__ == '__main__':
+    input_dim = 3
+    hidden_size = 5
+    output_size = 4
+    batch_size = 7
+    nc = NetClassifier()
+    params = get_init_params(input_dim, hidden_size, output_size)
+    X = np.random.randn(batch_size, input_dim)
+    Y = np.array([0, 1, 2, 0, 1, 2, 0])
+    nc.cost_grad(X, Y, params, reg=0)
+    test_grad()
